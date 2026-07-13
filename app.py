add556e4e11d8e385133a6712b63d838dc4d8002
Rituraj <YOUR_GITHUB_EMAIL>
2026-07-13 18:37:51 +0530

    Day 3 - Added Resume Details Module

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
 import streamlit as st
+from modules.pdf_reader import extract_text
+from modules.resume_details import get_resume_details
 
-# Page settings
 st.set_page_config(
     page_title="AI Resume Analyzer",
     page_icon="📄",
     layout="centered"
 )
 
-# Title
 st.title("📄 AI Resume Analyzer")
 
-# Welcome message
-st.write("Welcome Rituraj! 👋")
+st.error("VERSION 2 - TEST")
 
-# Description
-st.write("This is our AI-based Mini Project.")
+st.write("Welcome Rituraj 👋")
 
 uploaded_file = st.file_uploader(
-    "📄 Upload your Resume",
+    "Upload Resume",
     type=["pdf"]
 )
 
 if uploaded_file is not None:
+
     st.success("Resume Uploaded Successfully ✅")
+
+    resume_text = extract_text(uploaded_file)
+
+    details = get_resume_details(uploaded_file, resume_text)
+
+    st.subheader("📄 Resume Details")
+
+    st.divider()
+
+    st.subheader("📄 Extracted Resume Text")
+
+    st.text_area(
+        "Resume",
+        resume_text,
+        height=350
+    )
+
 else:
+
     st.warning("Please upload your resume.")
